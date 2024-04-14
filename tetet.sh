917c948b87edc8e193c2b56b745615478bf30da6
sagemantap <158444606+sagemantap@users.noreply.github.com>
2024-04-14 17:08:48 +0700

    Update tetet.sh

--- tetet.sh
+++ tetet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 POOL=pyrin.e4pool.com:12100
-WALLET=pyrin:qq92h3nryfwq0gkh73cwvjh9hhqlq2mank9sfxtgc99hqwn2ec6u2gszphr0u
+WALLET=sedra:qp3ldg8nys0385lheuk8c5vysxrgv3fl2wddd6j24htzs8sv6t7r7h0hel29x.Danis
 
 ./lolMiner --algo PYRIN --pool $POOL --user $WALLET $@
 
